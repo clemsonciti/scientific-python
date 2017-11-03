a7ee9a83cd90792c1cc80c5d19b56c0c13d36c19
Ashwin Srinath <ashwinsrnth@gmail.com>
2017-11-02 21:00:55 -0400

    Add tests for counting

---
+++ testing/solutions/counting-tests/test_counting.py
@@ -0,0 +1,37 @@
+from micro.counting import count_specimens
+import numpy as np
+from numpy.testing import assert_equal
+
+def test_count_no_object():
+    a = np.zeros([6, 6], dtype=np.float64)
+    assert_equal(count_specimens(a), 0)
+
+def test_count_1_object():
+    a = np.ones([6, 6], dtype=np.float64)
+    assert_equal(count_specimens(a), 1)
+
+def test_count_specimens_2_objects():
+    a = np.array(
+      [[ 0.,  0.,  0.,  0.,  0.,  0.,  0.],
+       [ 0.,  1.,  1.,  0.,  0.,  0.,  0.],
+       [ 0.,  1.,  1.,  0.,  0.,  0.,  0.],
+       [ 0.,  1.,  1.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  1.,  1.,  0.],
+       [ 0.,  0.,  0.,  0.,  1.,  1.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.]])
+    assert_equal(count_specimens(a), 2)
+
+def test_count_1_object_rectangle():
+    a = np.array(
+      [[ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  1.,  1.,  1.,  0.],
+       [ 0.,  1.,  1.,  1.,  0.],
+       [ 0.,  1.,  1.,  1.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.]])
+    assert_equal(count_specimens(a), 1)
