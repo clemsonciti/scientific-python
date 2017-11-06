a08ba0f64b4a1b8001c1c6db98c70d9ec8db73ce
Ashwin Srinath <atrikut@clemson.edu>
2017-11-06 14:30:59 -0500

    Update test_counting.py

--- testing/solutions/counting-tests/test_counting.py
+++ testing/solutions/counting-tests/test_counting.py
@@ -5,11 +5,18 @@ from numpy.testing import assert_equal
 def test_count_no_object():
     a = np.zeros([6, 6], dtype=np.int32)
     assert_equal(count_specimens(a), 0)
-
+    
 def test_count_1_object():
-    a = np.ones([6, 6], dtype=np.int32)
-    assert_equal(count_specimens(a), 1)
-
+    a = np.array(
+      [[ 0.,  0.,  0.,  0.,  0.,  0.,  0.],
+       [ 0.,  1.,  1.,  1.,  1.,  1.,  0.],
+       [ 0.,  1.,  1.,  1.,  1.,  1.,  0.],
+       [ 0.,  1.,  1.,  1.,  1.,  1.,  0.],
+       [ 0.,  1.,  1.,  1.,  1.,  1.,  0.],
+       [ 0.,  1.,  1.,  1.,  1.,  1.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.]], dtype=np.int32)
+    assert(count_objects(a) == 1)
+    
 def test_count_specimens_2_objects():
     a = np.array(
       [[ 0.,  0.,  0.,  0.,  0.,  0.,  0.],
